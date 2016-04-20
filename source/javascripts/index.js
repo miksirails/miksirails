59b0439826ad61f09238b74e9e43bfa2298634e6
Matias Korhonen <matias@kiskolabs.com>
2016-04-20 17:24:06 +0300

    Adds Hash navigation

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,10 +1,11 @@
 var bespoke = require("bespoke"),
     keys = require("bespoke-keys"),
     touch = require("bespoke-touch"),
+    hash = require('bespoke-hash'),
     nebula = require("bespoke-theme-nebula");
 
 $(function() {
-  var deck = bespoke.from("#presentation", [nebula(), keys(), touch()]);
+  var deck = bespoke.from("#presentation", [nebula(), keys(), touch(), hash()]);
 });
 
 if (__DEVELOPMENT__) {
