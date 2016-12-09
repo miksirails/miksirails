1b5ed5226ce45ae7297b3afa9ddbeac3b862f5d0
Simo Virtanen <simo@kiskolabs.com>
2016-12-09 16:45:23 +0200

    Implements the presentation information to the landing page for now
    Moves presentation to its own folder
    Changes fonts
    Adds jquery to make page scroll when user clicks "Read more" text

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -21,3 +21,49 @@ ready(function() {
 if (__DEVELOPMENT__) {
   console.log("Running in development mode!");
 }
+
+$(function() {
+    $("ul a")
+        .click(function(e) {
+            var link = $(this);
+
+            var item = link.parent("li");
+
+            if (item.hasClass("active")) {
+                item.removeClass("active").children("a").removeClass("active");
+            } else {
+                item.addClass("active").children("a").addClass("active");
+            }
+
+            if (item.children("ul").length > 0) {
+                var href = link.attr("href");
+                link.attr("href", "#");
+                setTimeout(function () {
+                    link.attr("href", href);
+                }, 300);
+                e.preventDefault();
+            }
+        })
+        .each(function() {
+            var link = $(this);
+            if (link.get(0).href === location.href) {
+                link.addClass("active").parents("li").addClass("active");
+                return false;
+            }
+        });
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 250);
+        return false;
+      }
+    }
+  });
+});
