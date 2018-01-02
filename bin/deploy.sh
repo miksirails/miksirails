e7f48bb370f62b1612763c9299323955433ffd1a
Matias Korhonen <matias@kiskolabs.com>
2018-01-02 14:58:06 +0200

    Use Upstatic for deploys instead of Stout

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,15 @@ function log {
 
 cd "${BASH_SOURCE%/*}/.." || exit
 
+echo `pwd`
+
+unamestr=`uname`
+if [[ "$unamestr" == "Linux" ]]; then
+  CMD="bin/upstatic-linux-x86_64"
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  CMD="bin/upstatic"
+fi
+
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
   log "Need to set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
   exit 1
@@ -16,9 +25,28 @@ if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   exit 1
 fi
 
+
+log "Setting up deployment configuration"
+
+tee -a "$HOME/.upstatic" <<EOF >/dev/null
+
+[accounts.miksirails]
+AccessKey = "${AWS_ACCESS_KEY_ID}"
+SecretKey = "${AWS_SECRET_ACCESS_KEY}"
+
+EOF
+
+if [ $? -eq 0 ]; then
+    log "Created Upstatic config file"
+else
+    log "Config file creation failed"
+    exit $?
+fi
+
+
 log "Starting deploy"
 
-stout deploy --key "$AWS_ACCESS_KEY_ID" --secret "$AWS_SECRET_ACCESS_KEY"
+./$CMD deploy
 
 if [ $? -eq 0 ]; then
     log "The deploy seems to have succeeded"
